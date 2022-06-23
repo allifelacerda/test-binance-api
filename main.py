9d6534c44a3b4d91e4ca0286f8eecdb4740b68d5
Allife <allife@msn.com>
2022-06-23 16:49:30 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from keys import api_key, api_secret
+from binance.client import Client
+import json
+from time import sleep
+
+SYMBOL = 'BTCUSDT'
+client = Client(api_key, api_secret)
+
+while True:
+    ticker = client.get_ticker(symbol=SYMBOL)
+    json_obj = json.dumps(ticker, indent=4)
+    print(json_obj)
+    sleep(1)
